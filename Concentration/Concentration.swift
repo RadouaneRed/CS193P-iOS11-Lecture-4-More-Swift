10214f1e25525a49a6627711ee04534ca94b253f
Michel Deiman <michel.deiman@live.nl>
2017-11-15 22:02:36 +0100

    Initial Commit

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,59 @@
+//
+//  Concentration.swift
+//  Lecture 4 - Concentration
+//
+//  Created by Michel Deiman on 13/11/2017.
+//  Copyright © 2017 Michel Deiman. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+	
+	private(set) var cards = [Card]()
+	private var indexOfOneAndOnlyFaceUpCard: Int? {
+		get {
+			var foundIndex: Int?
+			for index in cards.indices {
+				if cards[index].isFaceUp  {
+					guard foundIndex == nil else { return nil }
+					foundIndex = index
+				}
+			}
+			return foundIndex
+		}
+		set {
+			for index in cards.indices {
+				cards[index].isFaceUp = (index == newValue)
+			}
+		}
+				
+	}
+	
+	func chooseCard(at index: Int) {
+		assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)) : Choosen index out of range")
+		if !cards[index].isMatched {
+			if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+				// check if cards match
+				if cards[matchIndex] == cards[index] {
+					cards[matchIndex].isMatched = true
+					cards[index].isMatched = true
+				}
+				cards[index].isFaceUp = true
+			} else {
+				indexOfOneAndOnlyFaceUpCard = index
+			}
+		}
+	}
+	
+	init(numberOfPairsOfCards: Int) {
+		assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)) : You must have at least one pair of cards")
+		for _ in 1...numberOfPairsOfCards {
+			let card = Card()
+			cards += [card, card]
+		}
+	//	TODO: Shuffle the cards
+	}
+}
+
+
